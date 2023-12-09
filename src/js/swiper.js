6fdbdda887da731d76c59e1068ebf11ec55bd63f
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-12-09 12:01:28 +0200

    fix styles

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,8 +1,70 @@
-// //fix AK//
-// const swiper = new Swiper('.swiper', {
+//fix AK//
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+
+  //Slide per View 768px - 2.5 1200px - 4
+  slidesPerView: "auto",
+  spaceBetween: 16,
+
+  //Slide per 1200px - 4
+  slidesPerView: "auto",
+  spaceBetween: 28,
+});
+
+const swiper1 = new Swiper('.swiper1', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+
+  //Slide per View 768px - 2.5 1200px - 4
+  slidesPerView: "auto",
+  spaceBetween: 18,
+
+ })
+  
+// // Slider ver 2.0
+// if (window.matchMedia("(max-width: 767px)").matches) {
+//   // Viewport is more or equal to 768 pixels wide
+//  const swiper = new Swiper('.swiper', {
 //   // Optional parameters
 //   direction: 'horizontal',
-//   loop: false,
+//   loop: true,
 
 //   // If we need pagination
 //   pagination: {
@@ -10,30 +72,28 @@
 //     clickable: true,
 //   },
 
-//   // // Navigation arrows
-//   // navigation: {
-//   //   nextEl: '.swiper-button-next',
-//   //   prevEl: '.swiper-button-prev',
-//   // },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
 
-//   // // And if we need scrollbar
-//   // scrollbar: {
-//   //   el: '.swiper-scrollbar',
-//   // },
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
 
-//   //Slide per View 768px - 2.5 1200px - 4
-//   slidesPerView: "auto",
-//   spaceBetween: 16,
+//   //Slide per View
 
-//   //Slide per 1200px - 4
-//   slidesPerView: "auto",
-//   spaceBetween: 28,
-// });
+  
+//  })
 
-// const swiper1 = new Swiper('.swiper1', {
+// } else if (window.matchMedia("(max-width: 1199px)").matches) {
+//   // Viewport is more or equal to 768 pixels wide
+//  const swiper = new Swiper('.swiper', {
 //   // Optional parameters
 //   direction: 'horizontal',
-//   loop: false,
+//   loop: true,
 
 //   // If we need pagination
 //   pagination: {
@@ -41,153 +101,93 @@
 //     clickable: true,
 //   },
 
-//   // // Navigation arrows
-//   // navigation: {
-//   //   nextEl: '.swiper-button-next',
-//   //   prevEl: '.swiper-button-prev',
-//   // },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
 
-//   // // And if we need scrollbar
-//   // scrollbar: {
-//   //   el: '.swiper-scrollbar',
-//   // },
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
 
 //   //Slide per View 768px - 2.5 1200px - 4
-//   slidesPerView: "auto",
+//   slidesPerView: 2.5,
 //   spaceBetween: 18,
+  
+//  })
 
+// } else if (window.matchMedia("(max-width: 9999px)").matches) {
+//   // Viewport is more or equal to 768 pixels wide
+//  const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: false,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+
+//   //Slide per View 768px - 2.5 1200px - 4
+//   slidesPerView: 4,
+//   spaceBetween: 18,
+    
 //  })
   
-// // // Slider ver 2.0
-// // if (window.matchMedia("(max-width: 767px)").matches) {
-// //   // Viewport is more or equal to 768 pixels wide
-// //  const swiper = new Swiper('.swiper', {
-// //   // Optional parameters
-// //   direction: 'horizontal',
-// //   loop: true,
+// }
 
-// //   // If we need pagination
-// //   pagination: {
-// //     el: '.swiper-pagination',
-// //     clickable: true,
-// //   },
-
-// //   // Navigation arrows
-// //   navigation: {
-// //     nextEl: '.swiper-button-next',
-// //     prevEl: '.swiper-button-prev',
-// //   },
-
-// //   // And if we need scrollbar
-// //   scrollbar: {
-// //     el: '.swiper-scrollbar',
-// //   },
-
-// //   //Slide per View
-
-  
-// //  })
-
-// // } else if (window.matchMedia("(max-width: 1199px)").matches) {
-// //   // Viewport is more or equal to 768 pixels wide
-// //  const swiper = new Swiper('.swiper', {
-// //   // Optional parameters
-// //   direction: 'horizontal',
-// //   loop: true,
-
-// //   // If we need pagination
-// //   pagination: {
-// //     el: '.swiper-pagination',
-// //     clickable: true,
-// //   },
-
-// //   // Navigation arrows
-// //   navigation: {
-// //     nextEl: '.swiper-button-next',
-// //     prevEl: '.swiper-button-prev',
-// //   },
-
-// //   // And if we need scrollbar
-// //   scrollbar: {
-// //     el: '.swiper-scrollbar',
-// //   },
-
-// //   //Slide per View 768px - 2.5 1200px - 4
-// //   slidesPerView: 2.5,
-// //   spaceBetween: 18,
-  
-// //  })
-
-// // } else if (window.matchMedia("(max-width: 9999px)").matches) {
-// //   // Viewport is more or equal to 768 pixels wide
-// //  const swiper = new Swiper('.swiper', {
-// //   // Optional parameters
-// //   direction: 'horizontal',
-// //   loop: false,
-
-// //   // If we need pagination
-// //   pagination: {
-// //     el: '.swiper-pagination',
-// //     clickable: true,
-// //   },
-
-// //   // Navigation arrows
-// //   navigation: {
-// //     nextEl: '.swiper-button-next',
-// //     prevEl: '.swiper-button-prev',
-// //   },
-
-// //   // And if we need scrollbar
-// //   scrollbar: {
-// //     el: '.swiper-scrollbar',
-// //   },
-
-// //   //Slide per View 768px - 2.5 1200px - 4
-// //   slidesPerView: 4,
-// //   spaceBetween: 18,
-    
-// //  })
-  
 // // }
+// //lider ver 1.0
+//  const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: false,
 
-// // // }
-// // //lider ver 1.0
-// //  const swiper = new Swiper('.swiper', {
-// //   // Optional parameters
-// //   direction: 'horizontal',
-// //   loop: false,
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
 
-// //   // If we need pagination
-// //   pagination: {
-// //     el: '.swiper-pagination',
-// //     clickable: true,
-// //   },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
 
-// //   // Navigation arrows
-// //   navigation: {
-// //     nextEl: '.swiper-button-next',
-// //     prevEl: '.swiper-button-prev',
-// //   },
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
 
-// //   // And if we need scrollbar
-// //   scrollbar: {
-// //     el: '.swiper-scrollbar',
-// //   },
-
-// //   //Slide per View
-// //   slidesPerView: 4,
-// //   spaceBetween: 18,
+//   //Slide per View
+//   slidesPerView: 4,
+//   spaceBetween: 18,
   
-// //   effect: "coverflow",
-// //   grabCursor: true,
-// //   centeredSlides: true,
-// //   slidesPerView: "auto",
-// //   coverflowEffect: {
-// //   rotate: 50,
-// //   stretch: 0,
-// //   depth: 100,
-// //   modifier: 1,
-// //   slideShadows: true,
-// //   },
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   coverflowEffect: {
+//   rotate: 50,
+//   stretch: 0,
+//   depth: 100,
+//   modifier: 1,
+//   slideShadows: true,
+//   },
   
-// //  })
+//  })
