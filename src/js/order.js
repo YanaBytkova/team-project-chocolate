2b270bca7534dc5efe33fda60b1a11b6562d1db8
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-12-09 09:49:06 +0200

    fix error js

--- src/js/order.js
+++ src/js/order.js
@@ -1,14 +1,14 @@
-(() => {
-  const refs = {
-    openOrderBtn: document.querySelector("[data-order-open]"),
-    closeOrderBtn: document.querySelector("[data-order-close]"),
-    modal: document.querySelector("[data-order]"),
-  };
+// (() => {
+//   const refs = {
+//     openOrderBtn: document.querySelector("[data-order-open]"),
+//     closeOrderBtn: document.querySelector("[data-order-close]"),
+//     modal: document.querySelector("[data-order]"),
+//   };
 
-  refs.openOrderBtn.addEventListener("click", toggleOrder);
-  refs.closeOrderBtn.addEventListener("click", toggleOrder);
+//   refs.openOrderBtn.addEventListener("click", toggleOrder);
+//   refs.closeOrderBtn.addEventListener("click", toggleOrder);
 
-  function toggleOrder() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+//   function toggleOrder() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
